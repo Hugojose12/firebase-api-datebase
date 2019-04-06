29a26c4e3e9b29f2361273254e856c22385b8402
Hugo Rivero <Hugo200440@gmail.com>
2019-04-06 02:23:07 -0400

    first commit proyect auth firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import firebase from 'firebase';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyDhfF-JWfs7xit9gWpRA0y58NP6KvgvEMQ",
+	authDomain: "fir-app-b5dfe.firebaseapp.com",
+	databaseURL: "https://fir-app-b5dfe.firebaseio.com",
+	projectId: "fir-app-b5dfe",
+	storageBucket: "fir-app-b5dfe.appspot.com",
+	messagingSenderId: "587647173212"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
